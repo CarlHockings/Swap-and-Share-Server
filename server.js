ed964681d6e6bb2e6f0041de1e18652da31dd28f
Carl Hockings <gova@Carls-MacBook-Air.local>
2025-03-15 13:20:06 +0100

    Updated server file with magic link functionality - updated redirect link

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/send-magic-link", async (req, res) => {
             `${SUPABASE_URL}/auth/v1/magiclink`,
             {
                 email,
-                redirect_to: `https://swap-and-share-server.onrender.com/auth-user`,
+                redirect_to: `https://swap-and-share-server.onrender.com/auth-user?fix=true`, 
             },
             {
                 headers: {
